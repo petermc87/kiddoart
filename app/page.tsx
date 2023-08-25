565caebb0269ec8b0467b1a34c2b976e00830d49
petermc87 <peter.mcgibney@gmail.com>
2023-08-25 16:22:49 -0400

    Update responsiveness.

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <Container>
-      <main className={styles.main}>
-        <Container style={{ textAlign: "center" }}>
-          <SuggestionInput />
-        </Container>
-        <Container style={{ textAlign: "center" }}>
-          <GeneratedImage />
-        </Container>
-      </main>
-    </Container>
+    // <Container>
+    <main className={styles.main} style={{ padding: "2rem" }}>
+      <Container style={{ textAlign: "center", maxWidth: "30rem" }}>
+        <SuggestionInput />
+      </Container>
+      <Container style={{ textAlign: "center" }}>
+        <GeneratedImage />
+      </Container>
+    </main>
+    // </Container>
   );
 }
