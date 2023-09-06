8ee95b96087919097b5cfc3c1f9b82ac2aac8d49
petermc87 <peter.mcgibney@gmail.com>
2023-09-06 16:49:10 -0400

    feat: add prisma migrations and connecting to the database

---
+++ migrations/20230906202620_init-db.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex";
+
+// Populate the migrations with the schema in prisma.
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("images", (table) => {
+    table.string("id", 25).primary();
+    table.string("prompt", 100).notNullable();
+    table.string("url", 1000).notNullable();
+    table.timestamp("createdAt").defaultTo(knex.fn.now()).index();
+    table.timestamp("updatedAt").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("images");
+}
