084ae3dca25cd5099795a211376bb8c576dacbb1
petermc87 <peter.mcgibney@gmail.com>
2023-10-04 11:55:56 -0400

    feat: add chevron at the bottom of the list to show more previous imgages. 8 are shown on first render and 4 thereafter

--- app/components/GeneratedImages.tsx
+++ app/components/GeneratedImages.tsx
@@ -1,12 +1,19 @@
 import { ImageType } from "@/models/typings";
+import { useState } from "react";
 import { Card, Col, Image, Row } from "react-bootstrap";
-import styles from "./GeneratedImages.module.css";
+import styles from "./GeneratedImages.module.scss";
 
 type AllImagesProps = {
   images: ImageType[];
 };
 
 export default function GeneratedImages({ images }: AllImagesProps) {
+  // Create a state the stores the number of images to index.
+  //  This will mean that it will only render 8 at a time. When the chevron at
+  //  the bottom of the screen is clicked, 8 is added to the state, therefore
+  //  rendering 8 + 8 + ...
+  const [numOfImages, setNumOfImages] = useState(8);
+
   return (
     <>
       {images ? (
@@ -19,66 +26,50 @@ export default function GeneratedImages({ images }: AllImagesProps) {
             xxl={4}
             className="g-4"
           >
-            {images.map((image: any) => {
-              return (
-                <Col key={image.url}>
-                  <Card className="h-100">
-                    <Card.Body>
-                      <Card.Title style={{ font: "cursive" }}>
-                        Prompt
-                      </Card.Title>
-                      <Card.Text style={{ color: "black" }}>
-                        {image.prompt}
-                      </Card.Text>
-                    </Card.Body>
-                    <Image
-                      src={image.url}
-                      key={image.id}
-                      alt="image"
-                      rounded
-                      className={`card-img-top ${styles.image}`}
-                    />
-                  </Card>
-                </Col>
-              );
+            {images.map((image: any, i) => {
+              // Create a separate component for the image.
+              // To create the
+              if (i < numOfImages) {
+                return (
+                  // If the current index (i) is less than or equal to the state
+                  // value, then render to the screen.
+                  <Col key={image.url}>
+                    <Card className="h-100">
+                      <Card.Body>
+                        <Card.Title style={{ font: "cursive" }}>
+                          Prompt
+                        </Card.Title>
+                        <Card.Text style={{ color: "black" }}>
+                          {image.prompt}
+                        </Card.Text>
+                      </Card.Body>
+                      <Image
+                        src={image.url}
+                        key={image.id}
+                        alt="image"
+                        rounded
+                        className={`card-img-top ${styles.image}`}
+                      />
+                    </Card>
+                  </Col>
+                );
+              }
             })}
           </Row>
+          <></>
         </>
       ) : (
         ""
       )}
+      {/* On click will add to the current state i.e. current state + 4 */}
+      <div
+        className={styles.chevronWrapper}
+        onClick={() => {
+          setNumOfImages(numOfImages + 4);
+        }}
+      >
+        <div className={styles.chevron}></div>
+      </div>
     </>
   );
 }
-
-// ALTERNATIVE TO GETTING IMAGES //
-// try {
-//   const images = await getAllImages();
-//   setAllImages(images);
-// } catch (error) {
-//   console.error(error);
-// }
-
-// ORIGINAL SETUP FOR GETTING ALL IMAGES //
-// // Create state for images.
-// const [allImages, setAllImages] = useState<ImageType[] | void | null | any>(
-//   null
-// );
-
-// // ORIGINAL CODE FOR FETCHING ALL IMAGES //
-// // Create a hook for fetching.
-// useEffect(() => {
-//   // Create an async function within the hook.
-//   const images = async () => {
-//     try {
-//       // Return a promise and store the results in a variable.
-//       const results = await getAllImages();
-//       // Set the state
-//       setAllImages(results);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-//   // Call the function
-//   images();
-// }, []);
